0ee94e57ad4ce78ce10dc8aa719ad6cd872f28b0
tutkuince <tutku.ince@outlook.com>
2019-06-19 22:53:35 +0300

    findByCategoryName and findByDescription

--- src/main/java/com/muditasoft/recipemakerapp/repository/CategoryRepository.java
+++ src/main/java/com/muditasoft/recipemakerapp/repository/CategoryRepository.java
@@ -3,5 +3,8 @@ package com.muditasoft.recipemakerapp.repository;
 import com.muditasoft.recipemakerapp.model.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+    Optional<Category> findByCategoryName(String categoryName);
 }
--- src/main/java/com/muditasoft/recipemakerapp/repository/UnitOfMeasureRepository.java
+++ src/main/java/com/muditasoft/recipemakerapp/repository/UnitOfMeasureRepository.java
@@ -3,5 +3,9 @@ package com.muditasoft.recipemakerapp.repository;
 import com.muditasoft.recipemakerapp.model.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
