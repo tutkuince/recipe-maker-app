a85144a843722b135b0b1e441bda12085881a2ec
tutkuince <tutku.ince@outlook.com>
2019-06-19 21:40:17 +0300

    one to one relation with recipe

--- src/main/java/com/muditasoft/recipemakerapp/model/Notes.java
+++ src/main/java/com/muditasoft/recipemakerapp/model/Notes.java
@@ -1,5 +1,19 @@
 package com.muditasoft.recipemakerapp.model;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
 public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String recipeNotes;
+
+    @OneToOne
+    private Recipe recipe;
 }
