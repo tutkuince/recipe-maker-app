a7c84ca52100cc860f14416603f226f04e81f54f
tutkuince <tutku.ince@outlook.com>
2019-06-19 22:05:22 +0300

    Used @Enumerated for difficulty field

--- src/main/java/com/muditasoft/recipemakerapp/model/Recipe.java
+++ src/main/java/com/muditasoft/recipemakerapp/model/Recipe.java
@@ -29,4 +29,7 @@ public class Recipe {
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
+
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
 }
